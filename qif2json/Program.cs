1913411d215ce5e6107fe132949a528f6ce21277
egoroff <egoroff@gmail.com>
2015-04-29 08:56:53 +0300

    Command line utility draft

--- qif2json/Program.cs
+++ qif2json/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BurnSystems.CommandLine;
+using qif2json.parser;
 
 namespace qif2json
 {
@@ -10,6 +13,15 @@ namespace qif2json
     {
         static void Main(string[] args)
         {
+            var argument = Parser.ParseIntoOrShowUsage<ProgramArguments>(args);
+            if (argument == null)
+            {
+                return;
+            }
+            var qif = File.ReadAllText(argument.Input);
+            var parser = new QifParser();
+            parser.Compile(qif);
+            File.WriteAllText(argument.Output, parser.ToJson(true));
         }
     }
 }
---
+++ qif2json/ProgramArguments.cs
@@ -0,0 +1,16 @@
+﻿using BurnSystems.CommandLine.ByAttributes;
+
+namespace qif2json
+{
+    public class ProgramArguments
+    {
+        [UnnamedArgument(IsRequired = true, HelpText = "Path which will be compiled")]
+        public string Input { get; set; }
+
+        [UnnamedArgument(IsRequired = true, HelpText = "Path where compiled content will be stored.")]
+        public string Output { get; set; }
+
+        [NamedArgument(ShortName = 'v', HelpText = "Prints out more information")]
+        public bool Verbose { get; set; }
+    }
+}
