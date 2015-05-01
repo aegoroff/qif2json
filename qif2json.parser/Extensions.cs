244efe37d534cf6b06c082e9c0fd25d8b74fde2d
egoroff <egoroff@gmail.com>
2015-05-01 13:56:28 +0300

    File size output added

--- qif2json.parser/Extensions.cs
+++ qif2json.parser/Extensions.cs
@@ -3,7 +3,9 @@
 // © 2015 Alexander Egorov
 
 using System;
+using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace qif2json.parser
 {
@@ -23,6 +25,72 @@ namespace qif2json.parser
             where TInput : class
         {
             return input == null ? failure : evaluator(input);
-        } 
+        }
+
+        public static string FormatString(this ulong value)
+        {
+            if (value == 0)
+            {
+                return string.Empty;
+            }
+            Func<ulong, int> count = null;
+            count = num => (num /= 10) > 0 ? 1 + count(num) : 1;
+
+            var digits = count(value);
+
+            var builder = new StringBuilder();
+            for (var i = digits; i > 0; i--)
+            {
+                if (i % 3 == 0 && i < digits)
+                {
+                    builder.Append(' ');
+                }
+                builder.Append('#');
+            }
+            return builder.ToString();
+        }
+        private static string BytesToString(this ulong bytes)
+        {
+            return ((long)bytes).DeclensionEn("byte", "bytes");
+        }
+
+        private static string DeclensionEn(this long number, string nominative, string genitivePlural)
+        {
+            if (number == 1 || number == -1)
+            {
+                return nominative;
+            }
+            return genitivePlural;
+        }
+
+        private const string BigFileFormat = "{0:F2} {1} ({2} {3})";
+        private const string BigFileFormatNoBytes = "{0:F2} {1}";
+        private const string SmallFileFormat = "{0} {1}";
+
+        public static string Format(this FileSize fileSize)
+        {
+            string[] sizes =
+            {
+                "bytes",
+                "Kb",
+                "Mb",
+                "Gb",
+                "Tb",
+                "Pb",
+                "Eb"
+            };
+            if (fileSize.Unit == SizeUnit.Bytes)
+            {
+                return string.Format(CultureInfo.CurrentCulture, SmallFileFormat, fileSize.Bytes,
+                    fileSize.Bytes.BytesToString());
+            }
+            if (fileSize.BigWithoutBytes)
+            {
+                return string.Format(CultureInfo.CurrentCulture, BigFileFormatNoBytes, fileSize.Value, sizes[(int)fileSize.Unit]);
+            }
+            return string.Format(CultureInfo.CurrentCulture, BigFileFormat, fileSize.Value,
+                sizes[(int)fileSize.Unit], fileSize.Bytes.ToString(fileSize.Bytes.FormatString(), CultureInfo.CurrentCulture),
+                fileSize.Bytes.BytesToString());
+        }
     }
 }
---
+++ qif2json.parser/FileSize.cs
@@ -0,0 +1,98 @@
+// Created by: egr
+// Created at: 01.05.2015
+// © 2015 Alexander Egorov
+
+using System;
+
+namespace qif2json.parser
+{
+    /// <summary>
+    ///     Represents file size normalization code
+    /// </summary>
+    public struct FileSize
+    {
+        private const int Int64BitsCount = 64;
+        private const int BinaryThousand = 1024;
+
+        private readonly bool bigWithoutBytes;
+        private readonly ulong bytes;
+        private readonly SizeUnit sizeUnit;
+        private readonly double value;
+
+        /// <summary>
+        ///     Initializes new instance of <see cref="FileSize" /> structure using raw file size
+        /// </summary>
+        /// <param name="bytes">File size in bytes (signed)</param>
+        /// <param name="bigWithoutBytes">Whether to output bytes info if size is greater then 1024 bytes. False by default</param>
+        public FileSize(long bytes, bool bigWithoutBytes = false) : this((ulong)bytes, bigWithoutBytes)
+        {
+        }
+        
+        /// <summary>
+        ///     Initializes new instance of <see cref="FileSize" /> structure using raw file size
+        /// </summary>
+        /// <param name="bytes">File size in bytes</param>
+        /// <param name="bigWithoutBytes">Whether to output bytes info if size is greater then 1024 bytes. False by default</param>
+        public FileSize(ulong bytes, bool bigWithoutBytes = false)
+        {
+            this.bytes = bytes;
+            this.bigWithoutBytes = bigWithoutBytes;
+            this.sizeUnit = SizeUnit.Bytes;
+            this.sizeUnit = bytes == 0
+                ? SizeUnit.Bytes
+                : (SizeUnit)(IntegerLogarithm(bytes) / IntegerLogarithm(BinaryThousand));
+            this.value = this.sizeUnit == SizeUnit.Bytes ? 0 : bytes / Math.Pow(BinaryThousand, (int)this.sizeUnit);
+        }
+
+        /// <summary>
+        ///     Gets file size in bytes
+        /// </summary>
+        public ulong Bytes
+        {
+            get { return this.bytes; }
+        }
+
+        /// <summary>
+        ///     Gets the unit of value property
+        /// </summary>
+        public SizeUnit Unit
+        {
+            get { return this.sizeUnit; }
+        }
+
+        /// <summary>
+        ///     Gets normalized size defined by Unit property
+        /// </summary>
+        public double Value
+        {
+            get { return this.value; }
+        }
+
+        /// <summary>
+        /// Whether to output big values (> 1Kb) without bytes info
+        /// </summary>
+        public bool BigWithoutBytes
+        {
+            get { return this.bigWithoutBytes; }
+        }
+
+        private static ulong IntegerLogarithm(ulong x)
+        {
+            ulong n = Int64BitsCount;
+            var c = Int64BitsCount / 2;
+
+            do
+            {
+                var y = x >> c;
+                if (y != 0)
+                {
+                    n -= (ulong)c;
+                    x = y;
+                }
+                c >>= 1;
+            } while (c != 0);
+            n -= x >> (Int64BitsCount - 1);
+            return (Int64BitsCount - 1) - (n - x);
+        }
+    }
+}
